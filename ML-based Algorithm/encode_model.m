f04133487dfab0043014144f73436cab6b162209
yuanfu <y.fu-6@student.tudelft.nl>
2021-03-29 22:46:30 +0200

    version 1

---
+++ ML-based Algorithm/encode_model.m
@@ -0,0 +1,15 @@
+function [code]=encode_model(Model)
+    switch Model
+        case "Model-A"
+            code=1;
+        case "Model-B"
+            code=2;
+        case "Model-C"
+            code=3;
+        case "Model-D"
+            code=4;
+        case "Model-E"
+            code=5;
+        case "Model-F"
+            code=6;
+    end
